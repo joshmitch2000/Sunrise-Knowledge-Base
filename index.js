010ecd7b9b89df6907d9fa702b3a436a9c4d65d4
joshmitch2000 <joshoman123@hotmail.com>
2022-11-15 13:54:59 +1030

    made changes

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 window.onload = () => {
     let pageCont = document.getElementsByClassName("page-container");
 
-    let startOptions = document.createElement("section").classList.add("start-options");
+    let startOptions = document.createElement("section");
+    startOptions.classList.add("start-options");
 
-    let card1 = document.createElement("div").classList.add("option-card");
-    let card2 = document.createElement("div").classList.add("option-card");
-    let card3 = document.createElement("div").classList.add("option-card");
+    let card1 = document.createElement("div")
+    card1.classList.add("option-card");
+    let card2 = document.createElement("div");
+    card2.classList.add("option-card")
+    let card3 = document.createElement("div");
+    card3.classList.add("option-card")
 
-    let imageWrapper = document.createElement("div").classList.add("img-wrapper");
-    let powerLogo = document.createElement("img").classList.add("power-logo");
+    let imageWrapper = document.createElement("div");
+    imageWrapper.classList.add("img-wrapper");
+    let powerLogo = document.createElement("img");
+    powerLogo.classList.add("power-logo");
 
     startOptions.appendChild(card1);
     startOptions.appendChild(card2);
