9fadaa20a1868b8e1e4cf67d12435f8294adc07a
joshmitch2000 <joshoman123@hotmail.com>
2022-11-15 11:52:32 +1030

    added files

---
+++ index.js
@@ -0,0 +1,21 @@
+document.addEventListener("load", () => {
+    let pageCont = document.getElementsByClassName("page-container");
+    
+    let startOptions = document.createElement("section");
+    startOptions.classList.add("start-options");
+    // startOptions.style.display = "flex";
+    // startOptions.style.flexDirection = "row";
+    // startOptions.style.justifyContent = "space-around";
+    // startOptions.style.border = "1px solid lightgrey";
+    // startOptions.style.marginTop = "40px";
+    // startOptions.style.height = "250px";
+    // startOptions.style.padding = "0! important!";
+
+    let card = document.createElement("div");
+    card.classList.add("option-card");
+
+    startOptions.appendChild(card);
+    startOptions.appendChild(card);
+
+    pageCont[0].appendChild(startOptions);
+});
