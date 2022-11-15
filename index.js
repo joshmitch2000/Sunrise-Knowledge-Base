f8a85c349d5da1741bdb4975a7ce277f9672dcc0
joshmitch2000 <joshoman123@hotmail.com>
2022-11-15 12:33:50 +1030

    changed to window.onload

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
-document.getElementsByTagName("body")[0].addEventListener("load", () => {
+// document.getElementsByTagName("body")[0].addEventListener("load", () => {
+//     let pageCont = document.getElementsByClassName("page-container");
+    
+//     let startOptions = document.createElement("section");
+//     startOptions.classList.add("start-options");
+//     // startOptions.style.display = "flex";
+//     // startOptions.style.flexDirection = "row";
+//     // startOptions.style.justifyContent = "space-around";
+//     // startOptions.style.border = "1px solid lightgrey";
+//     // startOptions.style.marginTop = "40px";
+//     // startOptions.style.height = "250px";
+//     // startOptions.style.padding = "0! important!";
+
+//     let card = document.createElement("div");
+//     card.classList.add("option-card");
+
+//     startOptions.appendChild(card);
+//     startOptions.appendChild(card.cloneNode(true));
+
+//     pageCont[0].prepend(startOptions);
+// });
+
+window.onload = () => {
     let pageCont = document.getElementsByClassName("page-container");
     
     let startOptions = document.createElement("section");
@@ -18,7 +40,4 @@ document.getElementsByTagName("body")[0].addEventListener("load", () => {
     startOptions.appendChild(card.cloneNode(true));
 
     pageCont[0].prepend(startOptions);
-});
-
-
-// This is a test to test latest jsdelivr version
+}
