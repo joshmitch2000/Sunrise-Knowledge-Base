a0212961523b8b5b864d88b9ddfb44aa81fcc215
joshmitch2000 <joshoman123@hotmail.com>
2022-11-15 13:52:40 +1030

    made changes

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 window.onload = () => {
     let pageCont = document.getElementsByClassName("page-container");
-    
-    let startOptions = document.createElement("section");
-    startOptions.classList.add("start-options");
 
-    let card = document.createElement("div");
-    card.classList.add("option-card");
+    let startOptions = document.createElement("section").classList.add("start-options");
 
-    startOptions.appendChild(card);
-    startOptions.appendChild(card.cloneNode(true));
-    startOptions.appendChild(card.cloneNode(true));
+    let card1 = document.createElement("div").classList.add("option-card");
+    let card2 = document.createElement("div").classList.add("option-card");
+    let card3 = document.createElement("div").classList.add("option-card");
+
+    let imageWrapper = document.createElement("div").classList.add("img-wrapper");
+    let powerLogo = document.createElement("img").classList.add("power-logo");
+
+    startOptions.appendChild(card1);
+    startOptions.appendChild(card2);
+    startOptions.appendChild(card3);
 
     pageCont[0].prepend(startOptions);
 }
